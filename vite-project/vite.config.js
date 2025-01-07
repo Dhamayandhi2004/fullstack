d55cf33a52a697825bc4faa1d20394be975b4aab
Dhamayandhi2004 <126405025+Dhamayandhi2004@users.noreply.github.com>
2025-01-07 10:08:10 +0530

    commit the changes..

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -1,18 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist', // Ensure the output directory is correctly set for Vercel
-  },
-  server: {
-    base: '/', // Define the base path
-  },
-  resolve: {
-    alias: {
-      '@': '/src', // Optional: Add an alias for your source directory
-    },
-  },
-});
+})
